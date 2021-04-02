b95d40de7969a7abdd2e49740544d381d02a8a4e
JoyWambui <JoyWambui@github.com>
2021-04-02 13:45:46 +0300

    add search and landing page componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchResultsComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
