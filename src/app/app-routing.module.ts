55168536908218fed14d6ad37afab690237680df
JoyWambui <JoyWambui@github.com>
2021-04-05 15:37:31 +0300

    add routing to repositories component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { from } from 'rxjs';
-import {UsersComponent} from './users/users.component'
+import {UsersComponent} from './users/users.component';
+import {RepositoriesComponent} from './repositories/repositories.component';
 
 const routes: Routes = [
-  {path: 'users',component:UsersComponent}
+  {path: 'users',component:UsersComponent},
+  {path:'repos', component:RepositoriesComponent}
 ];
 
 @NgModule({
